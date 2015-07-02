afb3a00907437517a8c9d002054cfd1b01a83f4f
Tong Xiang <tong.xiang2@gmail.com>
2015-07-02 10:26:08 -0400

    Initial Commit

---
+++ photoSetter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  photoSetter
+//
+//  Created by Tong Xiang on 7/2/15.
+//  Copyright (c) 2015 DoSomething. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ photoSetter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  photoSetter
+//
+//  Created by Tong Xiang on 7/2/15.
+//  Copyright (c) 2015 DoSomething. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
