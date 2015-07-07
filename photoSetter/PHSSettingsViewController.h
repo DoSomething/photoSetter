6e1e925363e4315989ae69fa7b233206d6e6be91
Tong Xiang <tong.xiang2@gmail.com>
2015-07-07 19:31:17 -0400

    first pass at settings view

---
+++ photoSetter/PHSSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PHSSettingsViewController.h
+//  photoSetter
+//
+//  Created by Tong Xiang on 7/7/15.
+//  Copyright (c) 2015 DoSomething. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHSSettingsViewController : UIViewController
+
+@end
